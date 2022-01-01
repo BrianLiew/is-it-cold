e9369fda5379f6c525dab36bdb231292842c2346
Brian Liew <bliew1999@gmail.com>
2022-01-01 17:23:39 -0500

    started project

---
+++ is it cold/is it cold/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  is it cold
+//
+//  Created by Brian Liew on 1/1/22.
+//
+
+import SpriteKit
+import GameplayKit
+import UIKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
